e60d0e356a7b693ae14efaee93d7f6f763494de8
Zhou <peterzhouningbo@gmail.com>
2020-11-19 15:19:48 +0800

    天气App新建

---
+++ surron/android/app/src/main/java/com/zhoupeter/surron/MainActivity.java
@@ -0,0 +1,6 @@
+package com.zhoupeter.surron;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ surron/lib/WeatherWidget.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class WeatherWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _WeatherState();
+  }
+}
+
+class _WeatherState extends State<WeatherWidget> {
+    @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+
+    );
+  }
+}
